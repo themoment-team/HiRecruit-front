31c3157630c17a4e68e808122cebd4d8fff5c2db
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-05-31 11:57:38 +0900

    Add headers Access-Control-Allow-Headers key

--- src/libs/axios/axiosClient.ts
+++ src/libs/axios/axiosClient.ts
@@ -1,11 +1,15 @@
 import axios, { AxiosInstance as AxiosClient } from 'axios';
 
+const URL_PREFIX = '/api/v1';
+
 const axiosClient: AxiosClient = axios.create({
-  baseURL: process.env.baseUrl,
+  baseURL: process.env.baseUrl + URL_PREFIX,
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,DELETE,PATCH,PUT,OPTIONS',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials',
   },
 });
 

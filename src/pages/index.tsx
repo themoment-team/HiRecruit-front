d66e9d615aad1f0368bb95db1d130a93d73e0a9f
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-07 11:05:00 +0900

    Add primary meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,13 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
+        {/* Primary Meta Tags */}
         <title>HR | 하이리쿠르트</title>
+        <meta name="title" content="HR | 하이리쿠르트" />
+        <meta
+          name="description"
+          content="HiRecruit을 통해 보다 뛰어난 멘토를 찾고, 혁신적인 취업준비 경험을 느끼세요."
+        />
       </Head>
       <Map />
     </>

adf6beddefbfe4a86dd19f53845c2bda1fd08944
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-07 11:05:17 +0900

    Add Open Graph

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,31 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/static/HR_Transparent.png" type="image/png" />
+
+          {/* Open Graph / Facebook */}
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://hirecruit.site/" />
+          <meta property="og:title" content="HR | 하이리쿠르트" />
+          <meta
+            property="og:description"
+            content="HiRecruit을 통해 보다 뛰어난 멘토를 찾고, 혁신적인 취업준비 경험을 느끼세요."
+          />
+          <meta property="og:image" content="/static/더모먼트팀-OG.png" />
+
+          {/* Twitter */}
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://hirecruit.site/" />
+          <meta property="twitter:title" content="HR | 하이리쿠르트" />
+          <meta
+            property="twitter:description"
+            content="HiRecruit을 통해 보다 뛰어난 멘토를 찾고, 혁신적인 취업준비 경험을 느끼세요."
+          />
+          <meta property="twitter:image" content="/static/더모먼트팀-OG.png" />
+
+          {/* pretendard */}
+
           <link
             rel="stylesheet"
             type="text/css"

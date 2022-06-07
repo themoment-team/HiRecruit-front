135a98167915861e53f397c32b2538abacaac361
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-07 10:36:37 +0900

    Add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from 'next';
 
 import { Map } from 'components/Map';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-  return <Map />;
+  return (
+    <>
+      <Head>
+        <title>HR | 하이리쿠르트</title>
+      </Head>
+      <Map />
+    </>
+  );
 };
 
 export default Home;

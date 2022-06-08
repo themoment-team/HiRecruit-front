9eaadf993e343754467db2547200824ae72fde0d
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-08 17:38:34 +0900

    Div Button component style

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,26 +1,9 @@
-import styled from '@emotion/styled';
+import { ButtonHTMLAttributes } from 'react';
 
-import pallete from 'shared/Pallete';
+import * as S from './Button.styles';
 
-export const ButtonComponent = styled.button<{ secondary?: boolean }>`
-  border: none;
-  width: 100%;
-  height: 2.275rem;
-  text-align: center;
-  font-weight: 700;
-  font-size: 0.9rem;
-  border-radius: 0.563rem;
-  cursor: pointer;
-  color: ${pallete.scheme.white};
-  background-color: ${pallete.scheme.blue};
-  ${({ secondary }) =>
-    secondary &&
-    `
-      background-color: ${pallete.scheme.white};
-      color: ${pallete.scheme.blue};
-      border: 1px solid ${pallete.scheme.blue};
-    `};
-  &:active {
-    transform: scale(0.98);
-  }
-`;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const ButtonComponent: React.FC<ButtonProps> = props => {
+  return <S.Button {...props} />;
+};

ebd6bc06ea09e28f293c680cd4395238961797d3
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-11 22:52:16 +0900

    Update worker url

--- src/hooks/api/worker/use-worker-list.ts
+++ src/hooks/api/worker/use-worker-list.ts
@@ -2,13 +2,17 @@ import useSWR from 'swr';
 
 import apiClient from 'libs/axios/axiosClient';
 import { WorkerData } from 'types/worker.type';
+import { workerUrl } from 'libs/api/apiUrlControllers';
 
 interface UseWorkerList {
   data: WorkerData[];
 }
 
 const useWorkerList = () => {
-  const { data, error } = useSWR<UseWorkerList>(`/worker`, apiClient.get);
+  const { data, error } = useSWR<UseWorkerList>(
+    workerUrl.getAllWorker(),
+    apiClient.get,
+  );
 
   return {
     data: data?.data,

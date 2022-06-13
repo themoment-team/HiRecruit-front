93c71d54485cb4a1c155b427514758b8861bf22c
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-13 18:06:47 +0900

    Add Burger icon

---
+++ src/assets/icons/Burger.tsx
@@ -0,0 +1,16 @@
+import pallete from 'shared/Pallete';
+
+export const Burger: React.FC<React.SVGProps<SVGSVGElement>> = props => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill={pallete.scheme.white}
+      {...props}
+    >
+      <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+    </svg>
+  );
+};

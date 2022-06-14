1fd85c3f561fbd66d2b76229a378acdf4eb4939e
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-14 11:53:49 +0900

    Add flipX animation

--- src/shared/Animation.style.ts
+++ src/shared/Animation.style.ts
@@ -31,3 +31,29 @@ export const boundInDown = keyframes`
     transform: translate3d(0, 0, 0);
   }
 `;
+
+export const flipX = keyframes`
+  from {
+    transform: perspective(400px) rotate3d(1, 0, 0, 90deg);
+    animation-timing-function: ease-in;
+    opacity: 0;
+  }
+
+  40% {
+    transform: perspective(400px) rotate3d(1, 0, 0, -20deg);
+    animation-timing-function: ease-in;
+  }
+
+  60% {
+    transform: perspective(400px) rotate3d(1, 0, 0, 10deg);
+    opacity: 1;
+  }
+
+  80% {
+    transform: perspective(400px) rotate3d(1, 0, 0, -5deg);
+  }
+
+  to {
+    transform: perspective(400px);
+  }
+`;

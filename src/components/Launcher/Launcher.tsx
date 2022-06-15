ef19d5a230440704b6a335f28d694a5ebc380041
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-15 17:22:45 +0900

    Add link

--- src/components/Launcher/Launcher.tsx
+++ src/components/Launcher/Launcher.tsx
@@ -2,9 +2,11 @@ import * as S from './Launcher.styles';
 
 export const LauncherComponent: React.FC = () => {
   return (
-    <S.LauncherWrapper>
-      <S.LauncherText>프로젝트에 관하여</S.LauncherText>
-      <S.LauncherButton>?</S.LauncherButton>
-    </S.LauncherWrapper>
+    <a target="_blank" href="https://about.hirecruit.site/" rel="noreferrer">
+      <S.LauncherWrapper>
+        <S.LauncherText>프로젝트에 관하여</S.LauncherText>
+        <S.LauncherButton>?</S.LauncherButton>
+      </S.LauncherWrapper>
+    </a>
   );
 };

c821a250193e27decbed8dcdc8b1ad973c9ed50f
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-17 10:27:09 +0900

    Update register logic

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,7 +3,6 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { useSWRConfig } from 'swr';
 import toast from 'react-hot-toast';
 import { css } from '@emotion/react';
-import { useRouter } from 'next/router';
 
 import { Modal } from 'components/common/Modal';
 import { CompanyForm } from 'components/CompanyForm';
@@ -26,13 +25,7 @@ export const FormComponent: React.FC<SignUpFormProps> = ({
   const [companyFormModalVisible, setCompanyFormModalVisible] =
     useState<boolean>(false);
   const { mutate } = useSWRConfig();
-  const router = useRouter();
   const { data } = useCompanyList();
-
-  useEffect(() => {
-    router.replace(router.pathname);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const onSubmit: SubmitHandler<InputListType> = async data => {
     // TODO: post 로직 고도화
@@ -52,7 +45,7 @@ export const FormComponent: React.FC<SignUpFormProps> = ({
       .then(function (response) {
         toast.success('회원가입이 완료되었어요');
         mutate(workerUrl.getAllWorker());
-        setSignUpFormVisible(false);
+        window.location.reload();
       })
       .catch(function (error) {
         console.log(error);

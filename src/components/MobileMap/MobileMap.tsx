6642614d34047f1b77635887f4d38296763ec6b7
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-30 17:25:59 +0900

    Add handleLogout

--- src/components/MobileMap/MobileMap.tsx
+++ src/components/MobileMap/MobileMap.tsx
@@ -1,5 +1,3 @@
-import { Header } from 'components/common/Header';
-
 import { MobileWrapper } from './Mobile.styles';
 
 interface MobileMapProps {
@@ -9,15 +7,5 @@ interface MobileMapProps {
 }
 
 export const MobileMapComponent: React.FC<MobileMapProps> = ({ cookies }) => {
-  return (
-    <MobileWrapper>
-      <Header
-        userRules={'GUEST'}
-        menuVisible={false}
-        setMenuVisible={function (value: SetStateAction<boolean>): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-    </MobileWrapper>
-  );
+  return <MobileWrapper></MobileWrapper>;
 };

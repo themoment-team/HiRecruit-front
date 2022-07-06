e670f644678e655283d18c2d48098a15cf7656f0
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-07-06 11:32:44 +0900

    Add window size hook

---
+++ src/hooks/use-window-size.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+interface Size {
+  width: number | undefined;
+  height: number | undefined;
+}
+
+export const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState<Size>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+};

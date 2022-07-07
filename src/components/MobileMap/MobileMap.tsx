3e615453f50ce406a590009e3c777734264aedb4
songsihyeon <s20040@gsm.hs.kr>
2022-07-07 15:19:59 +0900

    Add react-spring-bottom-sheet

--- src/components/MobileMap/MobileMap.tsx
+++ src/components/MobileMap/MobileMap.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { MobileWrapper } from './Mobile.styles';
+import { BottomSheet } from 'react-spring-bottom-sheet';
 
 interface MobileMapProps {
   cookies: {
@@ -7,5 +9,12 @@ interface MobileMapProps {
 }
 
 export const MobileMapComponent: React.FC<MobileMapProps> = ({ cookies }) => {
-  return <MobileWrapper></MobileWrapper>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <MobileWrapper>
+      <button onClick={() => setOpen(true)}>open</button>
+      <BottomSheet open={open}>hi i am sihyeon</BottomSheet>
+    </MobileWrapper>
+  );
 };

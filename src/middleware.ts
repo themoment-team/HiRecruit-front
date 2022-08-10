8218240cc03578b8773b518bad8f940789757bfe
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-08-10 19:18:21 +0900

    Add getSiteState function

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,17 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-export async function middleware(req: NextRequest) {
+const getSiteState = async () => {
   const fetchUrl = `https://sheets.googleapis.com/v4/spreadsheets/${process.env.gssIdKey}/values/sheet1?key=${process.env.gssApiKey}`;
   const res = await fetch(fetchUrl);
   const data = await res.json();
-  const siteState = data?.values[1][0];
+
+  return data?.values[1][0];
+};
+
+export async function middleware(req: NextRequest) {
+  const siteState = await getSiteState();
 
   const { pathname, origin } = req.nextUrl;
-
   const { device } = userAgent(req);
   const viewport = device.type === 'mobile' ? 'mobile' : 'desktop';
 
